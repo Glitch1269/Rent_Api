4588d5202337df18b8ddaedb185fac404ba4c97f
Glitch1269 <111871384+Glitch1269@users.noreply.github.com>
2023-04-14 19:38:09 +0300

    Add files via upload

---
+++ View_Model/SonucModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentApiV2._0.View_Model
+{
+    public class SonucModel
+    {
+        public bool islem{ get; set; }
+        public string mesaj { get; set; }
+    }
+}
